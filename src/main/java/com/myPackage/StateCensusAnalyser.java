0054a9e95845ce90ed94afb9231a9d64c31b4dca
omkarmali4437 <omkarmalli688@gmail.com>
2021-03-20 20:09:32 +0530

    Added logic for loading data

---
+++ src/main/java/com/myPackage/StateCensusAnalyser.java
@@ -0,0 +1,35 @@
+package com.myPackage;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+public class StateCensusAnalyser {
+
+    public void loadData(String path){
+        int i=0;
+        try{
+            Reader reader= Files.newBufferedReader(Paths.get(path));
+            CsvToBean<CSVStateCensusAnalyser> csvToBean=new CsvToBeanBuilder(reader).withType(CSVStateCensusAnalyser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+
+            Iterator<CSVStateCensusAnalyser> csvStateCensusAnalyserIterator=csvToBean.iterator();
+
+            while(csvStateCensusAnalyserIterator.hasNext()){
+                CSVStateCensusAnalyser censusAnalyser=csvStateCensusAnalyserIterator.next();
+                System.out.println("Name : " + censusAnalyser.getState());
+                System.out.println("Email : " + censusAnalyser.getPopulation());
+                System.out.println("PhoneNo : " + censusAnalyser.getAreaInSqKm());
+                System.out.println("Country : " + censusAnalyser.getDensityPerSqKm());
+                System.out.println("==========================");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/myPackage/StateCensusAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.myPackage;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StateCensusAnalyserTest {
+    StateCensusAnalyser stateCensusAnalyser;
+    private String path="C:\\Users\\DELL\\IdeaProjects\\IndianStateCensus\\src\\main\\resources\\IndiaStateCensusData.csv";
+
+    @Before
+    public void setup() {
+        stateCensusAnalyser=new StateCensusAnalyser();
+    }
+
+    @Test
+    public void to_check_if_data_is_loaded(){
+        stateCensusAnalyser.loadData(path);
+    }
+}
